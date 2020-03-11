8127b3e8bf7f28c95015bb9e0af1ebd86ceb06e4
Hritik <hritik9870@gmail.com>
2020-03-11 10:20:05 +0530

    initial commit

--- index.js
+++ index.js
@@ -4,4 +4,14 @@
 const express = require("express");
 
 const e_app = express();
+
+
+// exam page
+e_app.get('/', function (req, res) {
+    res.sendFile( __dirname + '/public/exampage.html')
+  })
+
+
+
+
 e_app.listen(3000, () => console.log("Server up and running!"));
---
+++ public/pages/gamepage/question.js
@@ -0,0 +1,12 @@
+const questions = [{
+    question: 'what is the name of element with this symbol "O2"',
+    answers: [{
+        text: "Oxygen",
+        correct: true
+    },
+    {
+        text: "Carbon",
+        correct: false
+    }
+    ]
+}]
