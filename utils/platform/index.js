74cb64a2c4bf6b1374d33e7e18ad55340ffbb57a
LupusDiamond <godzplayer@hotmail.com>
2020-03-13 22:16:18 +0200

    added question template

--- utils/platform/index.js
+++ utils/platform/index.js
@@ -6,14 +6,17 @@ let testFile = JSON.parse(
   fs.readFileSync("./utils/platform/test.json").toString()
 );
 
-//testFile.name = "Peter";
-
-//fs.writeFile("./utils/platform/test.json", JSON.stringify(testFile));
-
 testFile.name = "Peter";
 //delete testFile.test;
 
-function addQuestion(initial, chapter) {}
+function addQuestion(initial, chapter) {
+  let questionTemplate = {
+    question: "",
+    answers: ["", "", ""],
+    correct: 0,
+    explaination: ""
+  };
+}
 function deleteQuestion(initial, chapter, idx) {}
 function editQuestion(initial, chapter, idx) {}
 
